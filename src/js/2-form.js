115e692e2e74e93be9058c2f5a40b9371318953e
Denys Almazov <almazovdenys@gmail.com>
2025-03-08 12:28:55 +0200

    init commit

---
+++ src/js/2-form.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_FORM_KEY = 'feedback-form-state';
+
+let formData = { email: '', message: '' };
+
+const fillFormFields = feedbackForm => {
+  try {
+    const formDataFromLS = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_FORM_KEY)
+    );
+
+    if (formDataFromLS === null) {
+      return;
+    }
+
+    formData = formDataFromLS;
+    const formDataKeys = Object.keys(formData);
+    formDataKeys.forEach(key => {
+      feedbackForm.elements[key].value = formData[key];
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fillFormFields(form);
+
+form.addEventListener('input', ({ target: formField }) => {
+  formData[formField.name] = formField.value;
+  localStorage.setItem(LOCAL_STORAGE_FORM_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  if (formData.message === '' || formData.email === '') {
+    return alert('Fill please all fields');
+  }
+  evt.target.reset();
+  localStorage.removeItem(LOCAL_STORAGE_FORM_KEY);
+});
